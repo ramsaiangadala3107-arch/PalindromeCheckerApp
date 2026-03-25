213153043d06f85d8d2dc6a44a3bbda3ad0dd3aa
ramsaiangadala3107-arch <ramsaiangadala3107@gmail.com>
2026-03-25 09:40:57 +0530

    Base application setup

--- src/PalindromeAppChecker.java
+++ src/PalindromeAppChecker.java
@@ -1,4 +1,7 @@
-package PACKAGE_NAME;
-
 public class PalindromeAppChecker {
+    public static void main(String[]args){
+        System.out.println("WELCOME TO PALINDROME CHECKER MANAGMENT SYSTEM");
+        System.out.println("Version:1.0");
+        System.out.println("System intialized successfully");
+    }
 }
